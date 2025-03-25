132f163a52122bbefc327d7d1fe7906027d46ef2
alirezaazimi326 <143959111+alirezaazimi326@users.noreply.github.com>
2025-03-26 02:22:45 +0330

    Update bot.py

--- barbarg_v2/api/bot.py
+++ barbarg_v2/api/bot.py
@@ -4,6 +4,15 @@ import subprocess
 @frappe.whitelist()
 def start_bot():
     try:
+        # Update all records in Drivers Log
+        frappe.db.sql("""
+            UPDATE `tabDrivers Log` 
+            SET situation = 'Pending', status = NULL, time_taken = NULL
+        """)
+        frappe.db.commit()
+
+        frappe.logger().info("Updated Drivers Log before starting the bot.")
+
         # Path to the directory containing your bot
         bot_directory = "/home/frappe/Barbarg-Ubuntu"
 
@@ -17,6 +26,7 @@ def start_bot():
         frappe.logger().info(f"Bot started with PID: {process.pid}")
 
         return {"message": "Bot started successfully", "pid": process.pid}
+    
     except Exception as e:
         frappe.logger().error(f"Failed to start bot: {str(e)}")
-        return {"message": f"Failed to start bot: {str(e)}"} 
+        return {"message": f"Failed to start bot: {str(e)}"}
